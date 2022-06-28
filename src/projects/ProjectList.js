ed12041008c7a4b494bf0678bcf880a4464350db
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-27 20:19:06 -0400

    lab07

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
 
 function ProjectList({ projects }) {
-  return <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>;
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+              <h5 className="strong">
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : {project.budget.toLocaleString()}</p>
+            </section>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 ProjectList.propTypes = {
