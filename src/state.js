c86c1fff5b70ede3302d3408d48951d876d602de
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-27 20:20:49 -0400

    lab23

---
+++ src/state.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { combineReducers } from 'redux';
+
+const reducer = combineReducers({});
+
+export default function configureStore(preloadedState) {
+  const middlewares = [ReduxThunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  //Thunk is middleware
+  //DevTools is an enhancer (actually changes Redux)
+  //applyMiddleware wraps middleware and returns an enhancer
+
+  // to use only thunk middleware
+  // const enhancer = compose(middlewareEnhancer);
+
+  //to use thunk & devTools
+  const enhancer = composeWithDevTools(middlewareEnhancer);
+
+  const store = createStore(reducer, preloadedState, enhancer);
+  return store;
+}
+
+export const initialAppState = {};
+
+export const store = configureStore(initialAppState);
