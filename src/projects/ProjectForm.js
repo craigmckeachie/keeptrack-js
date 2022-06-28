69be4bd1cd8ccd2f646469716361cb9dc8a318c3
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-27 20:19:13 -0400

    lab15

--- src/projects/ProjectForm.js
+++ src/projects/ProjectForm.js
@@ -1,22 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Project } from './Project';
 
-function ProjectForm({ onSave, onCancel }) {
+function ProjectForm({ project: initialProject, onSave, onCancel }) {
+  const [project, setProject] = useState(initialProject);
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSave(new Project({ name: 'Updated Project' }));
+    onSave(project);
   };
+
+  const handleChange = (event) => {
+    const { type, name, value, checked } = event.target;
+    // if input type is checkbox use checked
+    // otherwise it's type is text, number etc. so use value
+    let updatedValue = type === 'checkbox' ? checked : value;
+
+    //if input type is number convert the updatedValue string to a +number
+    if (type === 'number') {
+      updatedValue = Number(updatedValue);
+    }
+    const change = {
+      [name]: updatedValue,
+    };
+
+    let updatedProject;
+    // need to do functional update b/c
+    // the new project state is based on the previous project state
+    // so we can keep the project properties that aren't being edited +like project.id
+    // the spread operator (...) is used to
+    // spread the previous project properties and the new change
+    setProject((p) => {
+      updatedProject = new Project({ ...p, ...change });
+      return updatedProject;
+    });
+  };
+
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Project Name</label>
-      <input type="text" name="name" placeholder="enter name" />
+      <input
+        type="text"
+        name="name"
+        placeholder="enter name"
+        value={project.name}
+        onChange={handleChange}
+      />
       <label htmlFor="description">Project Description</label>
-      <textarea name="description" placeholder="enter description" />
+      <textarea
+        name="description"
+        placeholder="enter description"
+        value={project.description}
+        onChange={handleChange}
+      />
       <label htmlFor="budget">Project Budget</label>
-      <input type="number" name="budget" placeholder="enter budget" />
+      <input
+        type="number"
+        name="budget"
+        placeholder="enter budget"
+        value={project.budget}
+        onChange={handleChange}
+      />
       <label htmlFor="isActive">Active?</label>
-      <input type="checkbox" name="isActive" />
+      <input
+        type="checkbox"
+        name="isActive"
+        checked={project.isActive}
+        onChange={handleChange}
+      />
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
@@ -29,6 +79,7 @@ function ProjectForm({ onSave, onCancel }) {
 }
 
 ProjectForm.propTypes = {
+  project: PropTypes.instanceOf(Project),
   onCancel: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired,
 };
--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 
 function ProjectsPage() {
+  const [projects, setProjects] = useState(MOCK_PROJECTS);
   const saveProject = (project) => {
-    console.log('Saving project: ', project);
+    let updatedProjects = projects.map((p) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
   };
   return (
     <>
       <h1>Projects</h1>
-      <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
+      <ProjectList projects={projects} onSave={saveProject} />
     </>
   );
 }
