799abe620cc71496fcb891ffd12fdb0fc0cd5060
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-27 20:20:29 -0400

    lab18

--- src/projects/ProjectsPage.js
+++ src/projects/ProjectsPage.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+import React, { useState, useEffect } from 'react';
 import ProjectList from './ProjectList';
+import { projectAPI } from './projectAPI';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(MOCK_PROJECTS);
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(currentPage);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
   const saveProject = (project) => {
     let updatedProjects = projects.map((p) => {
       return p.id === project.id ? project : p;
@@ -13,7 +40,40 @@ function ProjectsPage() {
   return (
     <>
       <h1>Projects</h1>
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
       <ProjectList projects={projects} onSave={saveProject} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 }
