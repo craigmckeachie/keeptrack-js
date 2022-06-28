93d9bbc4c41cac4151bf8a989030205cf325988e
Craig McKeachie <craigdmckeachie@gmail.com>
2022-06-27 20:19:11 -0400

    lab13

--- src/projects/ProjectForm.js
+++ src/projects/ProjectForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ProjectForm() {
+function ProjectForm({ onCancel }) {
   return (
     <form className="input-group vertical">
       <label htmlFor="name">Project Name</label>
@@ -14,12 +15,16 @@ function ProjectForm() {
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
-        <button type="button" className="bordered medium">
+        <button type="button" className="bordered medium" onClick={onCancel}>
           cancel
         </button>
       </div>
     </form>
   );
 }
+
+ProjectForm.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+};
 
 export default ProjectForm;
